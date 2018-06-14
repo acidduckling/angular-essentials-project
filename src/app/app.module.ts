641ca833bf93860141ecd40f9b746f4030b68ba5
CLayton Wakefield <acidduckling@gmail.com>
2018-06-14 11:14:43 +1000

    Enable lazy loading of create character module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
-
-import { AppComponent } from './app.component';
-import { TabsComponent } from './tabs/tabs.component';
-import { ListComponent } from './list/list.component';
-import { ItemComponent } from './item/item.component';
-import { StarWarsService } from './star-wars.service';
-import { LogService } from './log.service';
-import { CreateCharacterComponent } from './create-character/create-character.component';
-import { HeaderComponent } from './header/header.component';
 import { HttpClientModule } from '@angular/common/http';
-
-const routes: Routes = [
-  {
-    path: 'characters',
-    component: TabsComponent,
-    children: [
-      { path: '', redirectTo: 'all', pathMatch: 'full' },
-      { path: ':side', component: ListComponent }
-    ]
-  },
-  { path: 'new-character', component: CreateCharacterComponent },
-  { path: '**', redirectTo: '/characters' }
-];
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ItemComponent } from './item/item.component';
+import { ListComponent } from './list/list.component';
+import { LogService } from './log.service';
+import { StarWarsService } from './star-wars.service';
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
   declarations: [
@@ -32,10 +16,9 @@ const routes: Routes = [
     TabsComponent,
     ListComponent,
     ItemComponent,
-    CreateCharacterComponent,
     HeaderComponent
   ],
-  imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot(routes)],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [StarWarsService, LogService],
   bootstrap: [AppComponent]
 })
---
+++ src/app/create-character/create-character.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CreateCharacterComponent } from './create-character.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [CreateCharacterComponent],
+  imports: [
+    FormsModule,
+    CommonModule,
+    RouterModule.forChild([{ path: '', component: CreateCharacterComponent }])
+  ],
+
+})
+export class CreateCharacterModule {}
